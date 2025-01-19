d65ca43490d48964033234ee49232bd9b5790a96
Bitter <yunsde18@gmail.com>
2025-01-19 10:21:24 -0500

    chore: new use cases

---
+++ Documents/hignition/src/app/portafolio/cases.ts
@@ -0,0 +1,63 @@
+export interface Case {
+    name: string;
+    category: string;
+    tags: string[];
+    image: string;
+}
+
+export const cases: Case[] = [
+    {
+        name: "Vitalut Saas",
+        category: "Salud",
+        tags: ["Branding", "Marketing", "Web"],
+        image: "/vita.png"
+    },
+    {
+        name: "Troveed",
+        category: "Tecnología",
+        tags: ["Diseño", "Branding", "Marketing"],
+        image: "/tro.png"
+    },
+    {
+        name: "Fitnopolis",
+        category: "e-Commerce",
+        tags: ["Marketing", "Diseño", "Videos"],
+        image: "/fit.png"
+    },
+    {
+        name: "Itaaj Realty ",
+        category: "Real State",
+        tags: ["Branding", "Marketing"],
+        image: "/ita.png"
+    },
+    {
+        name: "Sormi",
+        category: "Tecnología",
+        tags: ["Branding", "Marketing", "Web"],
+        image: "/sor.png"
+    },
+    {
+        name: "Prooving",
+        category: "e-Commerce",
+        tags: ["Marketing", "Diseño", "Videos"],
+        image: "/pro.png"
+    },   
+    {
+        name: "Helebba Saas",
+        category: "Tecnología",
+        tags: ["Marketing", "Diseño", "Videos"],
+        image: "/hel.png"
+    },   
+    {
+        name: "Algodonia",
+        category: "e-Commerce",
+        tags: ["Marketing", "Diseño", "Videos"],
+        image: "/alg.png"
+    },   
+    {
+        name: "Joobs",
+        category: "Tecnología",
+        tags: ["Marketing", "Diseño", "Videos"],
+        image: "/job.png"
+    },   
+]
--- Documents/hignition/src/app/portafolio/page.tsx
+++ Documents/hignition/src/app/portafolio/page.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import styles from "./Portfolio.module.css"
 import Growth from '@/sections/Home/Growth'
 import Image from 'next/image'
+import { cases } from './cases'
 
 const Portfolio = () => {
   return (
@@ -13,65 +14,23 @@ const Portfolio = () => {
             <h2>Proyectos exitosos, y nada más</h2>
 
             <div className={styles.list} >
+                {cases.map((project) => (
                 <div className={styles.card}>
-                    <picture>
-                    <Image src="/vita.png" layout='fill' alt='' />
-                    </picture>
-                    <div className={styles.content} >
-                        <h3>Vitalut Saas</h3>
-                        <p>Salud</p>
+                        <picture>
+                        <Image src={project.image} layout='fill' alt={project.name} />
+                        </picture>
+                        <div className={styles.content} >
+                            <h3>{project.name}</h3>
+                            <p>{project.category}</p>
 
-                        <div className={styles.tags}>
-                            <span>Branding</span>
-                            <span>Marketing</span>
-                            <span>Web</span>
+                            <div className={styles.tags}>
+                                {project.tags.map((tag) => (
+                                <span>{tag}</span>
+                                ))}
+                            </div>
                         </div>
-                    </div>
                 </div>
-                <div className={styles.card}>
-                    <picture>
-                    <Image src="/tro.png" layout='fill' alt='' />
-                    </picture>
-                    <div className={styles.content} >
-                        <h3>Trooved</h3>
-                        <p>Tecnología</p>
-
-                        <div className={styles.tags}>
-                            <span>Diseño</span>
-                            <span>Branding</span>
-                            <span>Marketing</span>
-                        </div>
-                    </div>
-                </div>
-                <div className={styles.card}>
-                    <picture>
-                        <Image src="/fit.png" layout='fill' alt='' />
-                    </picture>
-                    <div className={styles.content} >
-                        <h3>Fitnopolis</h3>
-                        <p>E-Commerce</p>
-
-                        <div className={styles.tags}>
-                            <span>Marketing</span>
-                            <span>Diseño</span>
-                            <span>Videos</span>
-                        </div>
-                    </div>
-                </div>
-                <div className={styles.card}>
-                    <picture>
-                    <Image src="/houl.webp" layout='fill' alt='' />
-                    </picture>
-                    <div className={styles.content} >
-                        <h3>Houlob</h3>
-                        <p>Real State</p>
-
-                        <div className={styles.tags}>
-                            <span>Branding</span>
-                            <span>Marketing</span>
-                        </div>
-                    </div>
-                </div>
+                ))} 
             </div>
         </section>
         <Growth />
